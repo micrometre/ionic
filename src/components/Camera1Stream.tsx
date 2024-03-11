d840995f7e8fe17258c1f83a5eab15195df63a9d
Henok <henokmicrometre@gmail.com>
2024-03-11 17:42:51 +0000

    added sse

--- src/components/Camera1Stream.tsx
+++ src/components/Camera1Stream.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 const Camera1Stream: React.FC<ContainerProps> = () => {
   return (
     <div >
-      <img  src="http://192.168.1.120:8080/video" width="375" height="281"></img>
+      <img  src="http://192.168.1.127:8080/video" width="375" height="281"></img>
     </div>
   );
 };
