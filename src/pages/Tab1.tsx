3e02e8efacc86cb17f9c00cdb16a3012f12f0188
Henok <henokmicrometre@gmail.com>
2024-03-17 18:01:26 +0000

    changed name

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -4,6 +4,16 @@ import Camera1Stream from '../components/Camera1Stream';
 import AlprStremPlates from '../components/AlprStream';
 import './Tab1.css';
 
+
+const onButtonClick = () => {
+  const link = document.createElement("a");
+  link.href = "http://192.168.1.122:5000/camera1/download";
+  link.download = "camera1.csv"; // specify the filename
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
 const Tab1: React.FC = () => {
   return (
     <IonPage>
@@ -19,6 +29,7 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
+        <IonButton color={'primary'} onClick={onButtonClick}> <a href="http://192.168.1.122:5000/camera1/download"></a>Export CSV</IonButton>
 
         <Camera1Stream name="Tab 1 page" />
 
@@ -37,7 +48,6 @@ const Tab1: React.FC = () => {
             marginLeft: 30,
             lineHeight: 1,
         }} src="http://192.168.1.122:5000/alprd1/mobile" ></iframe>
-        <IonButton color={'primary'}> <a href="http://127.0.0.1:5000/alprd1/download"></a>Export CSV</IonButton>
       </IonContent> 
     </IonPage>
   );
